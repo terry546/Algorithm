29648dfbcbffc65b2ee1cbc91c9166eecd70de3a
terry546 <vbcfgd45@naver.com>
2025-07-17 23:59:12 +0900

    soft commit
    입문 문제 다시 푸는중 6_2숫자 비교하기

--- programmers/1programmers_enter/6_2숫자비교하기.js
+++ programmers/1programmers_enter/6_2숫자비교하기.js
@@ -1,14 +1,22 @@
 function solution(num1, num2) {
-    if(num1 < num2){
-        let num= -1
-    }else if(num1 > num2){
-        let num= -1
+    let num=0
+    if(num1 != num2){
+        num= -1
     }else if(num1 == num2){
-        let num=1;
+        num=1;
     }
-    var answer = 0;
-    console.log(answer)
+    var answer = num;
     return answer;
 }
-
 solution(5, 10)
+
+// let solution2 =(num1,num2)=>{
+//     let num=0;
+//     if(num1 != num2){
+//         num= -1
+//     }else if(num1 == num2){
+//         num = 1
+//     }
+//     var answer = num
+//     return answer;
+// }
