32cbecd4d511510d484be41962a5b86f8ed38ba2
terry546 <vbcfgd45@naver.com>
2025-07-17 23:36:04 +0900

    6_2숫자비교하기
     6_2숫자비교하기 커밋

--- programmers/1programmers_enter/5_2두수의차.js
+++ programmers/1programmers_enter/5_2두수의차.js
@@ -1,5 +1,8 @@
-function solution(num1, num2) {
-    var answer = num1-num2;
-    console.log(answer)
-    return answer;
-}
+// function solution(num1, num2) {
+//     var answer = num1-num2;
+//     console.log(answer)
+//     return answer;
+// }
+
+let solution = (num1, num2) => num1-num2
+
---
+++ programmers/1programmers_enter/6_2숫자비교하기.js
@@ -0,0 +1,14 @@
+function solution(num1, num2) {
+    if(num1 < num2){
+        let num= -1
+    }else if(num1 > num2){
+        let num= -1
+    }else if(num1 == num2){
+        let num=1;
+    }
+    var answer = 0;
+    console.log(answer)
+    return answer;
+}
+
+solution(5, 10)
