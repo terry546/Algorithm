2eece2eaa5869bc99717a8eb42c095ef63a519f9
“terry546” <vbcfgd45@naver.com>
2026-02-07 16:09:47 +0900

    세균증식 복습, map으로 풀이

---
+++ programmers/1programmers_enter/22세균증식_2.js
@@ -0,0 +1,12 @@
+function solution(n, t) {
+    for(i=0; i<t; i++){
+        n = n * 2
+        answer = n
+        console.log(answer)
+    }
+    console.log(answer)
+    return answer;
+}
+
+
+solution(7,15)
---
+++ programmers/1programmers_enter/22세균증식_3map.js
@@ -0,0 +1,13 @@
+function solution(n, t) {
+    let answer = n;
+
+    // t만큼 반복하기 위해 길이가 t인 배열을 생성하고 map을 사용
+    new Array(t).fill(0).map(() => {
+        answer *= 2;
+        console.log(answer);
+    });
+
+    console.log(answer)
+    return answer;
+}
+solution(7,15)
