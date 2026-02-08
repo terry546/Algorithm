0bb069a0001402f5f7f2c7d470e10b52c8781aa4
“terry546” <vbcfgd45@naver.com>
2026-02-08 21:21:05 +0900

    중간저장

--- programmers/1programmers_enter/22세균증식_3map.js
+++ programmers/1programmers_enter/22세균증식_3map.js
@@ -3,7 +3,8 @@ function solution(n, t) {
 
     // t만큼 반복하기 위해 길이가 t인 배열을 생성하고 map을 사용
     new Array(t).fill(0).map(() => {
-        answer *= 2;
+        // answer *= 2;
+        answer = n* 2;
         console.log(answer);
     });
 
