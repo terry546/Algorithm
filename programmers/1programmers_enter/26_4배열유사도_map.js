2e5925a1019bcbf7c2f1c40d386dbf78fc1d7f31
“terry546” <vbcfgd45@naver.com>
2026-02-18 15:33:37 +0900

    26_4배열유사도_map 다시푸시

--- programmers/1programmers_enter/26_4배열유사도_map.js
+++ programmers/1programmers_enter/26_4배열유사도_map.js
@@ -1,9 +1,9 @@
 function solution(s1, s2) {
     var answer = 0;
-    s1.map(sc1 =>{
-        s2.map(sc2 =>{
-            if(sc1===sc2){
-                answer +=1
+    s1.map(sc1 => {
+        s2.map(sc2 => {
+            if (sc1 === sc2) {
+                answer += 1
                 console.log(answer)
             }
         })
@@ -17,4 +17,4 @@ function solution(s1, s2) {
 
 let s1 = ["a", "b", "c"]
 let s2 = ["com", "b", "d", "p", "c"]
-solution(s1,s2)
+solution(s1, s2)
