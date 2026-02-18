ff35fdf109ce7e9cb02bcfd33c7f5d896b0149bd
“terry546” <vbcfgd45@naver.com>
2026-02-18 15:18:26 +0900

    26_3배열유사도, 26_4배열유사도_map 풀이

---
+++ programmers/1programmers_enter/26_3배열유사도.js
@@ -0,0 +1,15 @@
+function solution(s1, s2) {
+    var answer = 0;
+    for (let i = 0; i < s2.length; i++) {
+        for (let j = 0; j < s1.length; j++) {
+            if (s1[j] === s2[i]) {
+                answer += 1
+                console.log(answer)
+            }
+        }
+    }
+    return answer;
+}
+let s1 = ["a", "b", "c"]
+let s2 = ["com", "b", "d", "p", "c"]
+solution(s1,s2)
---
+++ programmers/1programmers_enter/26_4배열유사도_map.js
@@ -0,0 +1,20 @@
+function solution(s1, s2) {
+    var answer = 0;
+    s1.map(sc1 =>{
+        s2.map(sc2 =>{
+            if(sc1===sc2){
+                answer +=1
+                console.log(answer)
+            }
+        })
+    })
+    return answer;
+}
+
+
+
+
+
+let s1 = ["a", "b", "c"]
+let s2 = ["com", "b", "d", "p", "c"]
+solution(s1,s2)
